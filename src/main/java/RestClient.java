b7525ede9045d9727d58bca414d02e232905f28d
Samuel_UCB <val97samuel@gmail.com>
2017-06-08 21:52:13 +0200

    First commit

--- src/main/java/RestClient.java
+++ src/main/java/RestClient.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 
 import org.apache.http.HttpResponse;
@@ -18,9 +19,15 @@ import org.apache.http.message.BasicNameValuePair;
 
 public class RestClient {
     public static void main(String[] args) {
-        try {
+        float[] longitudeAyer = {123, 12, 15, 16, 32, 16, 17, 72, 76, 10};
+        float[] latitudeAyer = {10, 51, 71, 86, 68, 14, 61, 56, 12, 34};
+        float[] longitudeHoy = {41, 61, 86, 12, 61, 71, 86, 12, 51, 16};
+        float[] latitudeHoy = {61, 17, 12, 61, 13, 71, 13, 16, 13, 14};
 
-            while(true){
+        try {                   // DAtos para hoy
+
+            int contador = 0;
+            while(contador < 10){
 
                 DefaultHttpClient httpClient = new DefaultHttpClient();
 
@@ -35,9 +42,16 @@ public class RestClient {
                 Integer  step = rand.nextInt(50) + 1;
                 Integer bpms = rand.nextInt(39)+52;
                 Integer distances = rand.nextInt(4)+1;
-                Float latitude = rand.nextFloat()*7+3;
-                Float longitude = rand.nextFloat()*10+5;
                 Integer calories =rand.nextInt(20)+5;
+
+                Float latitude = latitudeHoy[contador];
+                Float longitude = longitudeHoy[contador];
+
+                //latitude += 10000; <---------------- Agregar para que no se guarde la locacion
+                //longitude += 10000; <---------------- Agregar para que no se guarde la locacion
+                // Por lo menos debe existe una locacion pequeña Ej: (10,10) para que las lineas funcionen
+
+                // En el servidor aparecera "No se guardo una locacion"
 
                 postParameters.add(new BasicNameValuePair("steps",step.toString()));
                 postParameters.add(new BasicNameValuePair("bpm", bpms.toString()));
@@ -45,6 +59,7 @@ public class RestClient {
                 postParameters.add(new BasicNameValuePair("latitude", latitude.toString()));
                 postParameters.add(new BasicNameValuePair("longitude", longitude.toString()));
                 postParameters.add(new BasicNameValuePair("calories", calories.toString()));
+                postParameters.add(new BasicNameValuePair("fecha_registro", new Date().toString()));
                 postParameters.add(new BasicNameValuePair("user", "2"));
 
                 postRequest.setEntity(new UrlEncodedFormEntity(postParameters));
@@ -71,11 +86,8 @@ public class RestClient {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                contador++;
             }
-
-
-
-
         } catch (ClientProtocolException e) {
 
             e.printStackTrace();
@@ -84,6 +96,81 @@ public class RestClient {
 
             e.printStackTrace();
         }
+
+        try {           // <-------------------------------------- Datos para Ayer
+
+            int contador = 0;
+            while(contador < 10){
+
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+
+                HttpPost postRequest = new HttpPost(
+                        "http://localhost:8080/band");
+
+                ArrayList<NameValuePair> postParameters;
+
+                postParameters = new ArrayList<NameValuePair>();
+                Random rand = new Random();
+
+                Integer  step = rand.nextInt(50) + 1;
+                Integer bpms = rand.nextInt(39)+52;
+                Integer distances = rand.nextInt(4)+1;
+                Integer calories =rand.nextInt(20)+5;
+
+                Float latitude = latitudeAyer[contador];
+                Float longitude = longitudeAyer[contador];
+
+                //latitude += 10000; <---------------- Agregar para que no se guarde la locacion
+                //longitude += 10000; <---------------- Agregar para que no se guarde la locacion
+                // Por lo menos debe existe una locacion pequeña Ej: (10,10) para que las lineas funcionen
+
+                // En el servidor aparecera "No se guardo una locacion"
+
+                postParameters.add(new BasicNameValuePair("steps",step.toString()));
+                postParameters.add(new BasicNameValuePair("bpm", bpms.toString()));
+                postParameters.add(new BasicNameValuePair("distance", distances.toString()));
+                postParameters.add(new BasicNameValuePair("latitude", latitude.toString()));
+                postParameters.add(new BasicNameValuePair("longitude", longitude.toString()));
+                postParameters.add(new BasicNameValuePair("calories", calories.toString()));
+                postParameters.add(new BasicNameValuePair("fecha_registro", new Date(System.currentTimeMillis()-24*60*60*1000).toString()));
+                postParameters.add(new BasicNameValuePair("user", "2"));
+
+                postRequest.setEntity(new UrlEncodedFormEntity(postParameters));
+                postRequest.addHeader("accept", "application/json");
+
+                HttpResponse response = httpClient.execute(postRequest);
+
+                if (response.getStatusLine().getStatusCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : "
+                            + response.getStatusLine().getStatusCode());
+                }
+
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader((response.getEntity().getContent())));
+
+                String output;
+                System.out.println("Output from Server .... \n");
+                while ((output = br.readLine()) != null) {
+                    System.out.println(output);
+                }
+                httpClient.getConnectionManager().shutdown();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                contador++;
+            }
+        } catch (ClientProtocolException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+
 
     }
 }
